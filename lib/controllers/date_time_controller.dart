f59406612b357bd12a26ef25370ca029dcbf8e02
PatelVed671 <118421398+PatelVed671@users.noreply.github.com>
2023-08-26 11:45:38 +0530

    | VP | 26/08/23 | 11:45 | Added DateTime Controller...

---
+++ lib/controllers/date_time_controller.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class DateTimeController extends GetxController {
+  DateTime dateTime = DateTime.now();
+  TimeOfDay time = TimeOfDay.now();
+
+  selectDate() {
+
+  }
+}
